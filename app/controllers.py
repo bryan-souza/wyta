7af4e588ecb9c5409633ee68ecc4016279235578
bryan-souza <bryans.souza@gmail.com>
2022-11-23 18:07:39 -0300

    feat: implemented report controller

--- app/controllers.py
+++ app/controllers.py
@@ -1,6 +1,7 @@
+import re
 from pathlib import Path
-
 from tinydb import TinyDB
+from collections import Counter
 
 from app.config import CONFIG
 from app.exceptions import ItemNotFoundError
@@ -31,3 +32,35 @@ class ItemController:
     def insert(self, item: Item):
         self._db.insert(dict(item))
         return item
+
+
+class ReportController:
+
+    def __init__(self):
+        self._words = []
+        self._users = []
+
+    def create_report(self, digests):
+        normal_pattern = r'([0-9]{2}\/[0-9]{2}/[0-9]{4}\ [0-9]+:[0-9]{2}.+)\ -\ (.*?):\ (.+)'
+
+        for _digest in digests:
+            filepath = Path(CONFIG.storage_path, _digest)
+            with open(filepath, 'r', encoding='utf-8') as _file:
+                for _line in _file.readlines():
+                    match = re.match(normal_pattern, _line)
+                    if match is None:
+                        continue
+
+                    _, user, message = match.group(1, 2, 3)
+
+                    if user not in self._users:
+                        self._users.append(user)
+
+                    self._words.extend(message.split())
+
+        counter = Counter(self._words)
+
+        return {
+            'users': self._users,
+            'top_words': counter.most_common(10)
+        }
--- app/routers.py
+++ app/routers.py
@@ -4,21 +4,21 @@ from typing import List
 from fastapi import APIRouter, UploadFile, Depends, HTTPException
 from fastapi import status
 
-from app.controllers import ItemController, StorageController
+from app.controllers import ItemController, StorageController, ReportController
 from app.exceptions import ItemNotFoundError
 from app.models import Item
 
 files_router = APIRouter(prefix='/files')
 
 
-@files_router.post('/', status_code=status.HTTP_201_CREATED)
+@files_router.post('/', status_code=status.HTTP_200_OK)
 async def upload_file(
         files: List[UploadFile],
         item_controller: ItemController = Depends(),
-        storage_controller: StorageController = Depends()
+        storage_controller: StorageController = Depends(),
+        report_controller: ReportController = Depends()
 ):
-    conflicts = []
-    output = []
+    digests = []
 
     for file in files:
         _content = await file.read()
@@ -26,19 +26,12 @@ async def upload_file(
         _filename = file.filename
 
         try:
-            item_controller.get_by_digest(_digest)
-            # If it gets past this, it means the file already exists
-            # if so, append it to the conflicts list
-            conflicts.append(_filename)
+            _item = item_controller.get_by_digest(_digest)
         except ItemNotFoundError:
             storage_controller.write(_content, filename=_digest)
             _item = item_controller.insert(Item(digest=_digest, filename=_filename))
-            output.append(_item)
+        finally:
+            digests.append(_item.digest)
             continue
 
-    if len(conflicts) == len(files):
-        # Only raise error if all uploaded files conflict
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT)
-
-    # In case everything goes right, return a list of uploaded items
-    return output
+    return report_controller.create_report(digests)
